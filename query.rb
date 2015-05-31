c512235e01f789aed2dddda7123f2407cc752e48
Ervin Chow <ervinchow@gmail.com>
2015-05-31 15:43:43 -0700

    adds thor cli toolkit example

--- query.rb
+++ query.rb
@@ -2,6 +2,8 @@
 
 require 'td'
 require 'td-client'
+require 'thor'
+
 cln = TreasureData::Client.new(ENV['TREASURE_DATA_API_KEY'])
 job = cln.query('testdb', 'SELECT COUNT(1) FROM www_access')
 until job.finished?
@@ -10,4 +12,29 @@ until job.finished?
 end
 job.update_status!  # get latest info
 job.result_each { |row| p row }
+ 
+class MyCLI < Thor
+    class_option :verbose, :type => :boolean
 
+    desc "hello NAME", "say hello to NAME"
+    option :from, :default => "Ervin"
+    option :yell, :type => :boolean
+    def hello(name)
+        puts "> saying hello" if options[:verbose]
+        output = []
+        output << "from: #{options[:from]}" if options[:from]
+        output << "Hello #{name}"
+        output = output.join("\n")
+        puts options[:yell] ? output.upcase : output
+        puts "> done saying hello" if options[:verbose]
+    end
+    
+    desc "goodbye", "say goodbye to the world"
+    def goodbye
+        puts "> saying goodbye" if options[:verbose]
+        puts "Goodbye World"
+        puts "> done saying goodbye" if options[:verbose]
+    end
+end
+ 
+MyCLI.start(ARGV)

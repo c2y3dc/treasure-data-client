24f4b97bf17a1fcac6b1986cd6bc42b6772a78e7
Ervin Chow <ervinchow@gmail.com>
2015-05-31 17:06:14 -0700

    adds simple tests

--- spec/query_spec.rb
+++ spec/query_spec.rb
@@ -1,1 +1,12 @@
 require_relative '../query'
+require 'spec_helper'
+
+describe Query do
+    it "should raise" do
+        expect(TdCLI.start()).to raise_error("Query Failed")
+    end
+
+    it "should not raise" do
+        expect(TdCLI.start("sample_datasets www_access")).to_not raise_error("Query Failed")
+    end
+end

b15c629adb46f9688bd7fc38b2fd5c5796728a73
Ervin Chow <ervinchow@gmail.com>
2015-05-31 23:12:00 -0700

    adds more tests and changes to files to make tests run properly

---
+++ tdcli.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+
+require 'td'
+require 'td-client'
+require 'thor'
+require 'terminal-table'
+require 'csv'
+
+class TdCLI < Thor
+    default_task :my_default
+    
+        option :column, :default => "*", :aliases => :c
+        option :min, :default => "NULL", :aliases => :m
+        option :MAX, :default => "NULL", :aliases => :M
+        option :format, :default => "tabular", :aliases => :f
+        option :engine, :default => :hive, :aliases => :e
+
+        desc "td_default", "td_default method for querying db"
+        def td_default(*args)
+            db_name = args[0]
+            table_name = args[1]
+
+            cln = TreasureData::Client.new(ENV['TREASURE_DATA_API_KEY'])
+            # https://github.com/treasure-data/td-client-ruby/blob/master/lib/td/client.rb#L179
+            job = cln.query(db_name, "SELECT #{options[:column]} FROM #{table_name} WHERE TD_TIME_RANGE(time, #{options[:min]}, #{options[:MAX]})", nil, nil, nil, {:type => options[:engine].to_sym})
+            until job.finished?
+                sleep 1
+                job.update_progress!
+            end
+            job.update_status!  # get latest info
+
+            # See if it's CSV or tabular and ouput it
+            if options[:format] == "csv"
+                csv_string = CSV.generate do |csv|
+                    job.result_each { |row| csv << row }
+                end
+                puts options[:column] #headings
+                puts csv_string
+            else
+                puts Terminal::Table.new :rows => job.result, :headings => options[:column].split(',')
+            end
+            job.result
+        end 
+
+        # Work around for running Thor without explicitly calling method
+        def method_missing(method, *args)
+            args = ["td_default", method.to_s] + args
+            TdCLI.start(args)
+        end
+end
+ 
+# TdCLI.start(ARGV)
+
